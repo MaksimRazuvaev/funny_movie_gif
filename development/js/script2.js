a727b0efe5c7a9f2c26a4091f507e454106a8b95
Claire Bain <claire.bain3@gmail.com>
2022-11-03 21:31:33 -0700

    working on javascript event listeners and dom hooks

---
+++ development/js/script2.js
@@ -0,0 +1,14 @@
+
+// hooks to DOM
+
+var searchBtnEl = document.getElementById('btn'); // check if this ID is right
+var movieTitleEl = document.getElementById('TBD')
+var movieYearEl
+
+// event listeners
+
+searchBtnEl.addEventListener("click", function (event){
+    // display the search html
+    window.location = 'index2.html' // whatever this html is is
+
+})
