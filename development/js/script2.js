845ae31a9e11498fa5fb5e5ed5d8b47d4c2eaefe
Claire Bain <claire.bain3@gmail.com>
2022-11-07 19:18:34 -0800

    adds javascript dom hooks and event listeners

--- development/js/script2.js
+++ development/js/script2.js
@@ -1,14 +1,30 @@
 
 // hooks to DOM
+var searchFilterEl = document.getElementById('TBD') // this is the drop down on the seach form
+var searchBtnEl = document.getElementById('btn'); // this is the search button
+var movieTitleEl = document.getElementById('TBD') // this is the movie title
+var movieYearEl = document.getElementById('TBD') // this is the movie year
+var goBackEl = document.getElementById('TBD') // this is the back button
+var searchNameEl = document.getElementById('TBD') // this is the text field on form
 
-var searchBtnEl = document.getElementById('btn'); // check if this ID is right
-var movieTitleEl = document.getElementById('TBD')
-var movieYearEl
+// declare variables
+// variables to be passed to the API
+var searchType = searchFilterEl.value;
+var searchWord = searchNameEl.value;
+
+
+
 
 // event listeners
 
 searchBtnEl.addEventListener("click", function (event){
-    // display the search html
+    // display the search results html page 
     window.location = 'index2.html' // whatever this html is is
+
+})
+
+goBackEl.addEventListener("click", function (event){
+    // display the main search html page 
+    window.location = 'index.html' // whatever this html is is
 
 })
