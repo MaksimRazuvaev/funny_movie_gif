56404768177e0499a795bcb51642ea1cd5096d00
Claire Bain <claire.bain3@gmail.com>
2022-11-07 21:34:38 -0800

    added query selector for gif array

--- development/js/script2.js
+++ development/js/script2.js
@@ -1,23 +1,20 @@
 
 // hooks to DOM
 // var searchFilterEl = document.getElementById('TBD') // this is the drop down on the seach form
-var searchBtnEl = document.getElementById('btn'); // this is the search button
-var movieTitleEl = document.getElementById('title') // this is the movie title
-var movieYearEl = document.getElementById('year') // this is the movie year
-var movieRatedEl = document.getElementById('rated') // this is the movie year
-var movieReleasedEl = document.getElementById('released') // this is the movie released
-var movieGenreEl = document.getElementById('genre') // this is the movie released
-var movieDirectorEl = document.getElementById('director') // this is the movie released
-var movieRuntimeEl = document.getElementById('runtime') // this is the movie released
-var movieRatingsEl = document.getElementById('ratings') // this is the movie released
-var moviePlotEl = document.getElementById('plot') // this is the movie released
+var searchBtnEl = document.getElementById('btn'); 
+var movieTitleEl = document.getElementById('title') 
+var movieYearEl = document.getElementById('year') 
+var movieRatedEl = document.getElementById('rated') 
+var movieReleasedEl = document.getElementById('released') 
+var movieGenreEl = document.getElementById('genre') 
+var movieDirectorEl = document.getElementById('director')
+var movieRuntimeEl = document.getElementById('runtime') 
+var movieRatingsEl = document.getElementById('ratings') 
+var moviePlotEl = document.getElementById('plot') 
 var goBackEl = document.getElementById('TBD') // this is the back button
-// var searchNameEl = document.getElementById('TBD') // this is the text field on form
 
-// declare variables
-// variables to be passed to the API
-// var searchType = searchFilterEl.value;
-// var searchWord = searchNameEl.value;
+var gifCollectionEl = document.querySelectorAll('img');
+
 
 
 
